c441df3b517a27b4158bfd8b0c42270795baa724
Jufebrown <jufebrown@gmail.com>
2016-11-30 13:56:16 -0600

    got rid of bad characters

--- songs.js
+++ songs.js
@@ -6,5 +6,25 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs.unshift("Original Gangster by Ice-T on the album Original Gangster")
-songs.push("Fucc the Devil by Andrew Jackson Jihad on the album Knife Man")
+// Each student must add one song to the beginning and the end of the array.
+songs.unshift("Original Gangster > by Ice-T on the album Original Gangster")
+songs.push("Fucc the Devil > by Andrew Jackson Jihad on the album Knife Man")
+
+console.log(songs);
+
+// Loop over the array and remove any words or characters that obviously don't belong.
+function removeBadChar() {
+	for (var i = songs.length - 1; i >= 0; i--) {
+	 	songs[i] = songs[i].split("@").join("");
+	 	songs[i] = songs[i].split("*").join("");
+	 	songs[i] = songs[i].split("!").join("");
+	 	songs[i] = songs[i].split("(").join("");
+	}
+}
+
+removeBadChar();
+console.log(songs);
+
+// Students must find and replace the > character in each item with a - character.
+// Must add each string to the DOM in index.html in the main content area.
+
