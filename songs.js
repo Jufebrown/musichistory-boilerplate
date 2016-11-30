9c43fffe991dfaf4f52e80dac1d7922ed9a84f29
Jufebrown <jufebrown@gmail.com>
2016-11-30 13:23:18 -0600

    added song to beginning and end of the array

--- songs.js
+++ songs.js
@@ -5,3 +5,6 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+songs.unshift("Original Gangster by Ice-T on the album Original Gangster")
+songs.push("Fucc the Devil by Andrew Jackson Jihad on the album Knife Man")
