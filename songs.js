d60839055732a1aeb9bfff1e30869226ddc69f87
Jufebrown <jufebrown@gmail.com>
2016-12-13 15:09:31 -0600

    deleted console logs

--- songs.js
+++ songs.js
@@ -10,7 +10,6 @@ songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little
 songs.unshift("Original Gangster > by Ice-T on the album Original Gangster")
 songs.push("Fucc the Devil > by Andrew Jackson Jihad on the album Knife Man")
 
-console.log(songs);
 
 // Loop over the array and remove any words or characters that obviously don't belong.
 function removeBadChar() {
@@ -25,28 +24,25 @@ function removeBadChar() {
 }
 
 removeBadChar();
-console.log(songs);
+
 
 // Must add each string to the DOM in index.html in the main content area.
 // {Song name} by {Artist} on the album {Album}
 
 
 // function to break down strings into song, artist, and album variables
-function musicBreakdown() {	
+function musicBreakdown() {
 	for (var i = songs.length - 1; i >= 0; i--) {
 		var songArray = songs[i].split(" - by ");
 		song = songArray[0];
-		console.log(song);
 		var artistArray = songArray[1].split(" on the album ");
 		artist = artistArray[0];
-		console.log(artist);
 		var album = artistArray[1];
-		console.log(album);
 		var newSong = document.createElement('article');
 		newSong.innerHTML = '<h1>' + song + '</h1><ul><li>' + artist + '</li><li>' + album + '</li><li></li></ul>';
 		document.getElementById('songBody').appendChild(newSong);
-		
-		
+
+
 	}
 }
 
